d6498b350bc7d54a4a3d4339792e3368077165f4
scott-ha <haneul9209@gmail.com>
2021-04-02 14:28:48 +0900

    0402 95%

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 router.get('/ui', function(req, res, next) {
-  res.render('ui-components', { title: 'Express' });
+  res.render('ui-components');
 });
 
 router.get('/diagram', function(req, res, next) {
-  res.render('diagram', { title: 'Express' });
+  res.render('diagram');
+});
+
+router.get('/chart', function(req, res, next) {
+  res.render('chart');
+});
+
+router.get('/table', function(req, res, next) {
+  res.render('table');
 });
 
 module.exports = router;
